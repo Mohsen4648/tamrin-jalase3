13b0e6216f92c5e118b28a7b9971d58aad76b43e
Mohsen Noorbakhsh <smnby0@gmail.com>
2022-12-17 00:49:38 +0330

    you can calculate power of a number with two arguman

---
+++ POW.py
@@ -0,0 +1,8 @@
+import sys
+
+#first arguman is base and a double Number
+#second arguman is power and is positive or negetive integer
+a=float(sys.argv[1])
+b=int(sys.argv[2])
+"""python3 POW.py base power"""
+print (a**b)
