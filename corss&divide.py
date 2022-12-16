543b31ae3008a04e1dfa4466f49f0718da7fd382
Mohsen Noorbakhsh <smnby0@gmail.com>
2022-12-16 17:02:07 +0330

    change to functional

--- corss&divide.py
+++ corss&divide.py
@@ -2,12 +2,28 @@ def give():
     a=int(input("give me a number :"))
     return a
 
+def cross_divide(a,b):
+    
+    if b!=0:
+        c=float((a*b)-(a/b))
+        return c
+    else :
+        print ("choose another number")
+ 
 a=give()
 b=give()
 
-print ("a*b = " + str(a*b))
-print ("a/b = " + str(a/b))
-print ("now diffrent between a*b & a/b :")
-print (str((a/b)((b^2)-1)))
-      #"""(a*b) - (a/b) === (a/b)*( b^2  - 1)"""
+            
+#print ("a*b = " + str(a*b))
+#print ("a/b = " + str(a/b))
+#print ("now diffrent between a*b & a/b :")
+#print (str((a/b)((b^2)-1)))
+     # """(a*b) - (a/b) === (a/b)*( b^2  - 1)"""
+
+
+c=cross_divide(a,b)
+print (str(c))
+
+
+
       
