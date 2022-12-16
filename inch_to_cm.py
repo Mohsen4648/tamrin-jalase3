616bb7e8583063652cd10121ecdfb42da9a91848
Mohsen Noorbakhsh <smnby0@gmail.com>
2022-12-16 16:13:35 +0330

    base of plan for this project

---
+++ inch_to_cm.py
@@ -0,0 +1,30 @@
+def give():
+    """for give numbers"""
+    pass 
+
+def givechoice():
+    """choice which form to transfer"""
+    pass
+
+def inch2cm():
+    """every inch is .... cm"""
+    pass
+
+def cm2inch():
+    """every cm is .... inch"""
+    pass
+
+def foot2inch():
+    """every foot is .... inch"""
+    pass
+
+def inch2foot():
+    """every inch is .... foot"""
+    pass
+
+#match choice
+#"""now switch to which function"""
+#   case foot2inch()
+#   case inch2foot()
+#   case inch2cm()
+#   case cm2inch()
